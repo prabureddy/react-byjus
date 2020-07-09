b9c235f107b3c0100de5351123625ef7f45d332b
Bhargava Prabu Reddy <44055337+prabureddy@users.noreply.github.com>
2020-07-09 10:32:02 +0530

    changed index.js axios url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = "https://django-byjus-unnamed.herokuapp.com/";
   axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
